48ddcc43c1841dfd83861ab8ee179f46c3acce2d
preciousnjukia <preciousnjukia@gmail.com>
2023-07-08 16:47:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, giftDescription) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    const message = `Thank you, ${name}, for the wonderful ${giftDescription} gift!`;
+    thankYouMessages.push(message);
+  }
+
+  return thankYouMessages;
+}
+
+function countDown(number) {
+  for (let i = number; i>=0; i--) {
+    console.log(i);
+  }
+}
